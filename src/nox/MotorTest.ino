ab12e558501ace0437e82ae1e1857f88467b0bb0
BraedenCu <braedencullen120@gmail.com>
2020-09-01 20:55:49 -0500

    test

---
+++ src/nox/MotorTest.ino
@@ -0,0 +1,21 @@
+
+int enA = 8, in1 = 9, in2 = 10, enB = 11, in3 = 12, in4 = 13;
+
+void setup() {
+    //initialize motor controller
+    pinMode(enA, OUTPUT);
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+
+    pinMode(enB, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
+
+
+
+}
+
+void loop() {
+    
+
+}
