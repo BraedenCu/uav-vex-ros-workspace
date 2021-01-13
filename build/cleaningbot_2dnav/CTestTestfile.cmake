fa4fe6153b5c8864972acbe87eb2b78b6f860566
BraedenCu <braedencullen120@gmail.com>
2021-01-12 22:09:54 -0600

    inital commit

--- build/cleaningbot_2dnav/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dev/catkin_ws/src/cleaningbot_2dnav
-# Build directory: /home/dev/catkin_ws/build/cleaningbot_2dnav
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/gazeboSimulation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dev/catkin_ws/src/gazeboSimulation
-# Build directory: /home/dev/catkin_ws/build/gazeboSimulation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
