5414f5faa7b84fbdb98f6975f0b590fa020fcab9
BraedenCu <braedencullen120@gmail.com>
2021-01-12 22:11:45 -0600

    added encoder arduino script:

---
+++ src/vexai/arduinoScripts/encoderRosserialPublisher/encoderRosserialPublisher.ino
@@ -0,0 +1,99 @@
+// Use the following line if you have a Leonardo or MKR1000
+//#define USE_USBCON
+
+#include <ros.h>
+#include <vexai/Encoder.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Float32.h>
+#include <digitalWriteFast.h>  // library for high performance reads and writes by jrraines
+                               // see http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1267553811/0
+                               // and http://code.google.com/p/digitalwritefast/
+//All credits for the fantastic quaternion encoder logic go to Daniel J. Gonzalez (http://yameb.blogspot.com/2012/11/quadrature-encoders-in-arduino-done.html)
+                               
+// Quadrature encoders
+// Left encoder
+#define c_LeftEncoderInterruptA 0
+#define c_LeftEncoderInterruptB 1
+#define c_LeftEncoderPinA 2
+#define c_LeftEncoderPinB 3
+#define LeftEncoderIsReversed
+
+volatile bool _LeftEncoderASet;
+volatile bool _LeftEncoderBSet;
+volatile bool _LeftEncoderAPrev;
+volatile bool _LeftEncoderBPrev;
+volatile long _LeftEncoderTicks = 0;
+
+float rightEncoderData = 0.00;
+float leftEncoderData = 0.00;
+
+ros::NodeHandle nh;
+
+std_msgs::String str_msg;
+std_msgs::Float32 float_msg;
+ros::Publisher encoderData("encoderData", &float_msg);
+
+void setup()
+{
+  Serial.begin(9600);
+  //nh.initNode()j;
+  //nh.advertise(encoder);
+  // Quadrature encoders
+  // Left encoder
+  pinMode(c_LeftEncoderPinA, INPUT);      // sets pin A as input
+  digitalWrite(c_LeftEncoderPinA, LOW);  // turn on pullup resistors
+  pinMode(c_LeftEncoderPinB, INPUT);      // sets pin B as input
+  digitalWrite(c_LeftEncoderPinB, LOW);  // turn on pullup resistors
+  attachInterrupt(c_LeftEncoderInterruptA, HandleLeftMotorInterruptA, CHANGE);
+  attachInterrupt(c_LeftEncoderInterruptB, HandleLeftMotorInterruptB, CHANGE);
+  nh.initNode();
+  nh.advertise(encoderData);
+}
+
+void loop()
+{
+  leftEncoderData = _LeftEncoderTicks/8192.0;
+  float_msg.data = leftEncoderData;
+  encoderData.publish( &float_msg );
+  nh.spinOnce();
+  delay(10);
+}
+
+// Interrupt service routines for the left motor's quadrature encoder
+void HandleLeftMotorInterruptA(){
+  _LeftEncoderBSet = digitalReadFast(c_LeftEncoderPinB);
+  _LeftEncoderASet = digitalReadFast(c_LeftEncoderPinA);
+  
+  _LeftEncoderTicks+=ParseEncoder();
+  
+  _LeftEncoderAPrev = _LeftEncoderASet;
+  _LeftEncoderBPrev = _LeftEncoderBSet;
+}
+
+// Interrupt service routines for the right motor's quadrature encoder
+void HandleLeftMotorInterruptB(){
+  // Test transition;
+  _LeftEncoderBSet = digitalReadFast(c_LeftEncoderPinB);
+  _LeftEncoderASet = digitalReadFast(c_LeftEncoderPinA);
+  
+  _LeftEncoderTicks+=ParseEncoder();
+  
+  _LeftEncoderAPrev = _LeftEncoderASet;
+  _LeftEncoderBPrev = _LeftEncoderBSet;
+}
+
+int ParseEncoder(){
+  if(_LeftEncoderAPrev && _LeftEncoderBPrev){
+    if(!_LeftEncoderASet && _LeftEncoderBSet) return 1;
+    if(_LeftEncoderASet && !_LeftEncoderBSet) return -1;
+  }else if(!_LeftEncoderAPrev && _LeftEncoderBPrev){
+    if(!_LeftEncoderASet && !_LeftEncoderBSet) return 1;
+    if(_LeftEncoderASet && _LeftEncoderBSet) return -1;
+  }else if(!_LeftEncoderAPrev && !_LeftEncoderBPrev){
+    if(_LeftEncoderASet && !_LeftEncoderBSet) return 1;
+    if(!_LeftEncoderASet && _LeftEncoderBSet) return -1;
+  }else if(_LeftEncoderAPrev && !_LeftEncoderBPrev){
+    if(_LeftEncoderASet && _LeftEncoderBSet) return 1;
+    if(!_LeftEncoderASet && !_LeftEncoderBSet) return -1;
+  }
+}
